ac567c94ba3d7e3dcaa27d45b3c17986d4734f76
Savyasachichandel17 <161956334+Savyasachichandel17@users.noreply.github.com>
2024-03-02 20:43:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php  ?>
+
+<html>
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<title>CFARE Consultancy</title>
+	<!--/tags -->
+	
+</head>
+
+<body>
+<?php include('header.php');  ?>
+<!-- banner -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Improving workplace <span>Productivity.</span></h3>
+						<p>Hire. Train. Retain.</p>
+						<div class="agileits-button top_ban_agile">
+							<a class="btn btn-primary btn-lg" href="job-list.php">Explore Jobs »</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item item2">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Inspiring leadership <span>innovation.</span></h3>
+						<p>Hire. Train. Retain.</p>
+						<div class="agileits-button top_ban_agile">
+							<a class="btn btn-primary btn-lg" href="companies.php">Explore Companies »</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item item3">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Improving workplace <span>Productivity.</span></h3>
+						<p>Hire. Train. Retain.</p>
+						<div class="agileits-button top_ban_agile">
+							<a class="btn btn-primary btn-lg" href="job-list.php">Explore Jobs »</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item item4">
+				<div class="container">
+					<div class="carousel-caption">
+
+						<h3>Inspiring leadership <span>innovation.</span></h3>
+						<p>Hire. Train. Retain.</p>
+						<div class="agileits-button top_ban_agile">
+							<a class="btn btn-primary btn-lg scroll" href="companies.php" role="button">Explore Companies »</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="fa fa-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="fa fa-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		<!-- The Modal -->
+	</div>
+	<!--//banner -->
+
+	
+	
+	<!-- //banner-bottom -->
+	<!--<div class="team_work_agile">
+		<h4>Whether we play a large or small role, by working together we achieve our objectives.</h4>
+	</div>-->
+	<!-- services -->
+	<div class="services" id="services">
+		<div class="container">
+			<div class="tittle_head_w3ls">
+				<h3 class="tittle">Selection Process</h3>
+			</div>
+			<div class="inner_sec_grids_info_w3ls">
+				<div class="col-md-3 services-left">
+					<div class="services-left-top">
+
+						<h5>Identify Need & Develop Position Description</h5>
+
+					</div>
+					<div class="services-left-top services-left-top1">
+
+						<h5>Recruitment Planning</h5>
+
+					</div>
+				</div>
+				<div class="col-md-6 services-middle">
+					<div class="services-middle-img">
+						<img src="images/process.jpg" alt="" />
+					</div>
+					<div class="services-middle-grids">
+						<div class="col-md-6 services-middle-left">
+							<div class="services-left-top">
+
+								<h5>Sourcing & Advertising</h5>
+
+							</div>
+						</div>
+						<div class="col-md-6 services-middle-left">
+							<div class="services-left-top">
+
+								<h5>Assess & Interview Candidates</h5>
+
+							</div>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="col-md-3 services-left">
+					<div class="services-left-top">
+
+						<h5>Offer for Student Employment</h5>
+
+					</div>
+					<div class="services-left-top services-left-top1">
+
+						<h5>Onboarding for Success</h5>
+
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!-- //services -->
+	<!-- /mid-services -->
+	
+	<!-- //mid-services -->
+
+	<!-- //testimonials -->
+	<div class="testimonials_section">
+		<div class="container">
+			<div class="tittle_head_w3ls">
+				<h3 class="tittle">Testimonials</h3>
+			</div>
+			<div class="inner_sec_grids_info_w3ls">
+				<div id="Carousel" class="carousel slide two">
+
+					<ol class="carousel-indicators second">
+						<li data-target="#Carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#Carousel" data-slide-to="1"></li>
+						<li data-target="#Carousel" data-slide-to="2"></li>
+					</ol>
+
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+
+						<div class="item active">
+							<div class="testimonials_grid_wthree">
+								<img src="images/test1.jpg" alt=" " class="img-responsive" />
+								<h4><i class="fa fa-quote-left" aria-hidden="true"></i> Nam libero tempore, cum soluta nobis est eligendi optio cumque
+									nihil impedit quo minus id quod maxime placeat facere possimus,Morbi viverra congue nisi vel pulvinar posuere sapien
+									eros. omnis voluptas.</h4>
+								<h5>Dan Adams</h5>
+
+							</div>
+
+						</div>
+						<!--.item-->
+						<div class="item">
+							<div class="testimonials_grid_wthree">
+								<img src="images/test2.jpg" alt=" " class="img-responsive" />
+								<h4><i class="fa fa-quote-left" aria-hidden="true"></i> Nam libero tempore, cum soluta nobis est eligendi optio cumque
+									nihil impedit quo minus id quod maxime placeat facere possimus,Morbi viverra congue nisi vel pulvinar posuere sapien
+									eros. omnis voluptas.</h4>
+								<h5>Jessica Doe</h5>
+
+							</div>
+						</div>
+						<!--.item-->
+
+						<div class="item">
+							<div class="testimonials_grid_wthree">
+								<img src="images/test3.jpg" alt=" " class="img-responsive" />
+								<h4><i class="fa fa-quote-left" aria-hidden="true"></i> Nam libero tempore, cum soluta nobis est eligendi optio cumque
+									nihil impedit quo minus id quod maxime placeat facere possimus,Morbi viverra congue nisi vel pulvinar posuere sapien
+									eros. omnis voluptas.</h4>
+								<h5>Michael Doe</h5>
+
+							</div>
+						</div>
+						<!--.item-->
+
+					</div>
+					<!--.carousel-inner-->
+
+				</div>
+				<!--.Carousel-->
+
+			</div>
+		</div>
+	</div>
+
+
+<?php include('footer.php');  ?>
+</body>
+</html>
